41238f17cb96e49c231a95e8b669b4ad15c5c95e
clp510 <clp510@tju.edu.cn>
2015-01-08 21:51:48 +0800

    delete work file

--- rtl/tb/maf/work/lza_32/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity lza_32 is
-    port(
-        a               : in     vl_logic_vector(31 downto 0);
-        count           : out    vl_logic_vector(5 downto 0)
-    );
-end lza_32;
--- rtl/tb/maf/work/maf_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity maf_tb is
-end maf_tb;
